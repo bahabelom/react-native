086886138329865624ac92fe3be2d46e8590cea2
bahabelom <bahabelomgebremedhn2@gmail.com>
2026-01-09 16:33:57 +0300

    all setup first react app run

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// For Expo Go, use "main" as the app name
+// For standalone builds, this should match MainActivity.getMainComponentName()
+AppRegistry.registerComponent('main', () => App);
